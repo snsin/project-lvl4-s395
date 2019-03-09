8805e45abfc4177991dbe2cbdaba2f5db8ec71d5
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-03-09 12:38:51 +0300

    feat: add gulp tasks

---
+++ gulpfile.babel.js
@@ -0,0 +1,18 @@
+import gulp from 'gulp';
+import repl from 'repl';
+import container from './container';
+import getServer from '.';
+
+gulp.task('console', () => {
+  const replServer = repl.start({
+    prompt: 'Application console > ',
+  });
+
+  Object.keys(container).forEach((key) => {
+    replServer.context[key] = container[key];
+  });
+});
+
+gulp.task('server', (cb) => {
+  getServer().listen(process.env.PORT || 4000, cb);
+});
--- index.js
+++ index.js
@@ -4,41 +4,41 @@ import Router from 'koa-router';
 import koaWebpack from 'koa-webpack';
 import serve from 'koa-static';
 import path from 'path';
+import _ from 'lodash';
 import container from './container';
 import middlevares from './middlewares';
 import addRoutes from './routes';
 import webpackConfig from './webpack.config';
 
-const app = new Koa();
-const PORT = process.env.PORT || 3000;
+export default () => {
+  const app = new Koa();
+  const { errInform, logger } = middlevares;
+  app.use(logger(container));
+  app.use(errInform(container));
+  app.use(serve(path.join(__dirname, 'public')));
+  if (process.env.NODE_ENV !== 'production') {
+    koaWebpack({
+      config: webpackConfig,
+    }).then(m => app.use(m));
+  }
 
-
-const { errInform, logger } = middlevares;
-app.use(logger(container));
-app.use(errInform(container));
-app.use(serve(path.join(__dirname, 'public')));
-if (process.env.NODE_ENV !== 'production') {
-  koaWebpack({
-    config: webpackConfig,
-  }).then(m => app.use(m));
-}
-
-const router = new Router();
-addRoutes(router, container);
-app.use(router.allowedMethods());
-app.use(router.routes());
-const pug = new Pug({
-  viewPath: path.join(__dirname, 'views'),
-  // noCache: process.env.NODE_ENV === 'development',
-  debug: true,
-  pretty: true,
-  compileDebug: true,
-  locals: [],
-  basedir: path.join(__dirname, 'views'),
-  /* helperPath: [
-    { _ },
-    { urlFor: (...args) => router.url(...args) },
-  ], */
-});
-pug.use(app);
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  const router = new Router();
+  addRoutes(router, container);
+  app.use(router.allowedMethods());
+  app.use(router.routes());
+  const pug = new Pug({
+    viewPath: path.join(__dirname, 'views'),
+    noCache: process.env.NODE_ENV === 'development',
+    debug: true,
+    pretty: true,
+    compileDebug: true,
+    locals: [],
+    basedir: path.join(__dirname, 'views'),
+    helperPath: [
+      { _ },
+      { urlFor: (...args) => router.url(...args) },
+    ],
+  });
+  pug.use(app);
+  return app;
+};
