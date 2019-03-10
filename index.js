6abc5797663421ed7120de52536e118f1aeecdec
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-03-10 13:28:21 +0300

    fix: get webpack do not to build while testing

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ export default () => {
   app.use(logger(container));
   app.use(errInform(container));
   app.use(serve(path.join(__dirname, 'public')));
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV === 'development') {
     koaWebpack({
       config: webpackConfig,
     }).then(m => app.use(m));
