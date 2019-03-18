fb2df87ba930339aa7d09a274bbec0c2ad0fbf62
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-03-18 16:05:33 +0300

    feat: add implemented features to app

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import Pug from 'koa-pug';
 import Router from 'koa-router';
 import koaWebpack from 'koa-webpack';
 import serve from 'koa-static';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-generic-session';
+import flash from 'koa-flash-simple';
+import methodOverride from 'koa-methodoverride';
 import path from 'path';
 import _ from 'lodash';
 import container from './container';
@@ -12,9 +16,21 @@ import webpackConfig from './webpack.config';
 
 export default () => {
   const app = new Koa();
-  const { errInform, logger } = middlevares;
+  const { errInform, logger, sessionHelper } = middlevares;
+  app.keys = [process.env.APP_SESSIONS_KEY1, process.env.APP_SESSIONS_KEY2];
+  app.use(session(app));
+  app.use(flash());
+  app.use(sessionHelper);
   app.use(logger(container));
   app.use(errInform(container));
+  app.use(bodyParser());
+  app.use(methodOverride((req) => {
+    // return req?.body?._method;
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      return req.body._method; // eslint-disable-line
+    }
+    return null;
+  }));
   app.use(serve(path.join(__dirname, 'public')));
   if (process.env.NODE_ENV === 'development') {
     koaWebpack({
