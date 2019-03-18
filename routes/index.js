b528b4cb5768917a0ae3c511711926bb16873d07
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-03-18 16:00:38 +0300

    feat: add users and sessions routes to app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import welcome from './welcome';
 import error from './throw-err';
+import users from './users';
+import sessions from './sessions';
 
-const controllers = [welcome, error];
+const controllers = [welcome, error, users, sessions];
 
 export default (router, container) => controllers.forEach(f => f(router, container));
