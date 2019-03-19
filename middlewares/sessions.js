6e89842e4e31dc529e352730a248eaa4fbde8232
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-03-19 14:16:29 +0300

    feat: add user name to state

--- middlewares/sessions.js
+++ middlewares/sessions.js
@@ -1,6 +1,7 @@
 export default async (ctx, next) => {
   ctx.state = {
     flash: ctx.flash,
+    userName: ctx.session.userName,
     isSignedIn: () => ctx.session.userId !== undefined,
   };
   await next();
